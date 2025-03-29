8a12d12f3fa5310996b99699a7780289c791c119
Sondre071 <Sirciller@gmail.com>
2025-03-29 21:59:53 +0100

    Gigachad rewrite 1.0

--- ModuleData.psm1
+++ ModuleData.psm1
@@ -1,44 +1,54 @@
 class DataManager {
-    [string]$SettingsPath
-    [psobject]$Settings
+    [string]$FilePath
+    [psobject]$FileContent
 
     DataManager([string]$path) {
-        $this.SettingsPath = $path
+        $this.FilePath = $path
         $this.Load()
     }
 
     [void] Load() {
-        if (-not (Test-Path -Path $this.SettingsPath)) {
-            throw "Config file not found at $($this.SettingsPath)."
+        if (-not (Test-Path -Path $this.FilePath)) {
+            throw "Config file not found at $($this.FilePath)."
         }
 
         try {
-            $this.Settings = Get-Content -Path $this.SettingsPath -Raw | ConvertFrom-Json -Depth 7
+            $this.FileContent = Get-Content -Path $this.FilePath -Raw | ConvertFrom-Json -Depth 7
         }
         catch {
-            throw "Failed to parse settings file: $_."
+            throw "Failed to parse file: $_."
         }
     }
 
-    [void] SaveSettings() {
-        $this.Settings | ConvertTo-Json -Depth 7 | Set-Content -Path $this.SettingsPath
+    [void] Save() {
+        $this.FileContent | ConvertTo-Json -Depth 7 | Set-Content -Path $this.FilePath
     }
     
-    [void] AddSetting($Key, $Value) {
-        $this.Settings | Add-Member -MemberType NoteProperty -Name $Key -Value $Value -Force
+    [void] Add($Key, $Value) {
+        $this.FileContent | Add-Member -MemberType NoteProperty -Name $Key -Value $Value -Force
         $this.Save()
     }
 }
 
-function ModuleData($ModulePath) {
+function ModuleData() {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$ModulePath,
 
-    $settingsPath = Join-Path -Path $ModulePath -ChildPath 'settings.json'
+        [Parameter(Mandatory = $true)]
+        [string]$FileName
+    )
 
-    if (-not (Test-Path -Path $settingsPath)) {
-        @{} | ConvertTo-Json -Depth 7 | Set-Content -Path $settingsPath -Encoding UTF8
+    if (-not $ModulePath) { throw "Missing module path." }
+    if (-not $FileName) { throw "Missing file name." }
+
+    $filePath = Join-Path -Path $ModulePath -ChildPath "$FileName.json"
+
+    if (-not (Test-Path -Path $filePath)) {
+        @{} | ConvertTo-Json -Depth 7 | Set-Content -Path $filePath -Encoding UTF8
     }
 
-    return [DataManager]::new($settingsPath)
+    return [DataManager]::new($filePath)
 }
 
 Export-ModuleMember -Function ModuleData
