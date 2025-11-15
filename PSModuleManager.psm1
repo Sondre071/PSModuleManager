1e49f36982b5b52187346993e05e0c6e4105abab
Sondre071 <Sirciller@gmail.com>
2025-11-15 20:34:47 +0100

    Refactoring: Remove redundant methods, simplify

--- PSModuleManager.psm1
+++ PSModuleManager.psm1
@@ -1,21 +1,22 @@
 class ModuleManager {
     [string]$FilePath
-    [psobject]$FileContent
-    [string]$ScriptRoot
+    [pscustomobject]$FileContent
 
-    ModuleManager([string]$path, [string]$scriptRoot) {
-        $this.FilePath = $path
-        $this.ScriptRoot = $scriptRoot
+    ModuleManager([string]$Path) {
+        $this.FilePath = $Path
         $this.Load()
     }
 
     [void] Load() {
         if (-not (Test-Path -Path $this.FilePath)) {
-            throw "Failed to find config file at $($this.FilePath)."
+            throw "No file found at path: `'$($this.FilePath)`'."
         }
 
         try {
             $this.FileContent = Get-Content -Path $this.FilePath -Raw | ConvertFrom-Json -Depth 7
+
+            Write-Debug "Loaded file path: $($this.FilePath)"
+            Write-Debug "Loaded content: $($this.FileContent)"
         }
         catch {
             throw "Failed to parse file: $_."
@@ -24,113 +25,37 @@ class ModuleManager {
 
     [void] Save() {
         $this.FileContent | ConvertTo-Json -Depth 7 | Set-Content -Path $this.FilePath
-    }
-    
-    [object] GetFile([string]$Path) {
-        $content = Get-Content -Path (Join-Path -Path $this.ScriptRoot -ChildPath $Path) -Raw
-        return $content
-    }
 
-    [void] SetFile([string]$Path, $Value) {
-        $fullpath = Join-Path -Path $this.ScriptRoot -ChildPath $Path
-        if (-not (Test-Path -Path $fullpath)) {
-            New-Item -Path $fullPath -Force
-        }
-        Set-Content -Path $fullPath -Value $Value
-    }
-
-    [string[]] GetFileNames([string]$Path) {
-        $fullpath = Join-Path -Path $this.ScriptRoot -ChildPath $Path
-        $fileNames = (Get-ChildItem -Path $fullPath -File) | ForEach-Object { $_.BaseName }
-
-        return $fileNames
-    }
-
-    [object] Get([string[]]$PathKeys, [switch]$Force) {
-
-        if (-not ($PathKeys.Count -gt 1)) {
-            if (-not $Force) { $this.ValidateProperty($PathKeys[0]) }
-        }
-        else {
-            $this.ValidatePath($PathKeys, $Force)
-        }
-
-        $currentObj = $this.FileContent 
-
-        foreach ($key in $PathKeys) {
-            $currentObj = $currentObj.$key
-        }
-
-        return $currentObj
-    }
-
-    [void] Set([string[]]$PathKeys, $Value, [switch]$Force) {
-
-        if (-not ($PathKeys.Count -gt 1)) {
-            $key = $PathKeys[0]
-            if (-not $this.FileContent.$key) {
-                $this.FileContent | Add-Member -Membertype NoteProperty -Name $key -Value $Value -Force
-            }
-            $this.FileContent.$key = $Value
-        }
-        else {
-            $pathKeysWithoutLast = $PathKeys[0..($PathKeys.Count - 2)]
-            $this.ValidatePath($pathKeysWithoutLast, $Force)
-
-            $currentObj = $this.FileContent
-
-            for ($i = 0; $i -lt $PathKeys.Count - 1; $i++) {
-                $currentObj = $currentObj.$($PathKeys[$i])
-            }
-
-            $finalKey = $PathKeys[-1]
-            $currentObj.$finalKey = $Value
-        }
-
-        $this.Save()
-    }
-
-    [void] ValidateProperty([string]$Property) {
-        if (-not ($this.FileContent.PSObject.Properties.Name -contains $Property)) {
-            throw "Property $Property does not exist on $($this.FilePath)."
-        }
-    }
-    
-    [void] ValidatePath([string[]]$PathKeys, [switch]$Force) {
-        $currentObj = $this.FileContent
-
-        foreach ($key in $PathKeys) {
-            if ($Force -and -not $currentObj.$key) {
-                $currentObj | Add-Member -Membertype NoteProperty -Name $key -Value (@{}) -Force
-            }
-            elseif (-not ($currentObj.$key -is [hashtable] -or $currentObj.$key -is [psobject] -or $currentObj.$key -is [array])) {
-                throw "Failed to parse key: $key as a hashtable, PSObject or array."
-            }
-
-            $currentObj = $currentObj.$key
-        }
+        Write-Debug "Saved to file path: `'$($this.FilePath).`'"
+        Write-Debug "Saved content: `'$($this.FileContent)`'."
     }
 }
 
 function PSModuleManager() {
     param (
         [Parameter(Mandatory = $true)]
-        [string]$ScriptRoot,
+        [string]$FilePath,
 
-        [Parameter(Mandatory = $true)]
-        [string]$FileName
+        [Parameter(Mandatory = $false)]
+        [string]$InitialJSONContent
     )
 
-    if (-not $ScriptRoot) { throw "PSScriptRoot not provided." }
-    if (-not $FileName) { throw "File name not provided." }
+    if (-not ($FilePath -match ".json$")) { throw "File must be of type JSON." }
 
-    $filePath = Join-Path -Path $ScriptRoot -ChildPath "$FileName.json"
-
-    if (-not (Test-Path -Path $filePath)) {
-        @{} | ConvertTo-Json -Depth 7 | Set-Content -Path $filePath -Encoding UTF8
+    if (-not (Test-Path $FilePath)) {
+        if ($InitialJSONContent.GetType().Name -eq 'string') {
+            $content = $InitialJSONContent | ConvertFrom-Json -Depth 7 | ConvertTo-Json -Depth 7 -Compress
+            Set-Content -Path $filePath -Value $content -Encoding UTF8
+        }
+        else {
+            throw 'Invalid file path. No JSON provided as initial file content.'
+        }
     }
 
-    return [ModuleManager]::new($filePath, $ScriptRoot)
+    $currentFile = Split-Path $PSCommandPath -Leaf
+    Write-Debug "Initializing $currentFile"
+
+    return [ModuleManager]::new($FilePath)
 }
 
 Export-ModuleMember -Function PSModuleManager
